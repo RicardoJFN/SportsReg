ff9248f06fbef63ed2d7a338824b431c5354d9d9
Ricardo Nunes <ricardo.nunes.isla@gmail.com>
2018-09-17 23:32:56 +0100

    Added a empty controller. Added a Workout model and a Workout Db Context

---
+++ SportsReg/Data/WorkoutContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsReg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsReg.Data
+{
+    public class WorkoutContext : DbContext
+    {
+        public WorkoutContext(DbContextOptions<WorkoutContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Workout> Workouts { get; set; }
+    }
+}
---
+++ SportsReg/Models/Workout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsReg.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public int DistanceInMeters { get; set; }
+        public long TimeInSeconds { get; set; }
+    }
+}
