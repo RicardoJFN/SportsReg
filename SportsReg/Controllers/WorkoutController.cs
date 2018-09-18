638a5c9435e729067859e764a427d2b5ca8b3269
Ricardo Nunes <ricardo.nunes.isla@gmail.com>
2018-09-18 22:53:27 +0100

    Added the GetWorkouts and GetWorkout methods in the controller

--- SportsReg/Controllers/WorkoutController.cs
+++ SportsReg/Controllers/WorkoutController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SportsReg.Data;
+using SportsReg.Models;
 
 namespace SportsReg.Controllers
 {
@@ -11,5 +13,33 @@ namespace SportsReg.Controllers
     [ApiController]
     public class WorkoutController : ControllerBase
     {
+        private readonly WorkoutContext _context;
+
+        public WorkoutController(WorkoutContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Workouts
+        [HttpGet]
+        [Route("GetWorkouts")]
+        public IEnumerable<Workout> GetWorkouts()
+        {
+            return _context.Set<Workout>();
+        }
+
+
+        // GET: api/Workout/1
+        [HttpGet]
+        [Route("GetWorkout/{id}")]
+        public async Task<IActionResult> GetWorkoutAsync(int id)
+        {
+            Workout workout = _context.Set<Workout>().Find(id);
+
+            if (workout == null)
+                return NotFound($"No workout with the id: {id}");
+
+            return Ok(workout);
+        }
     }
 }
