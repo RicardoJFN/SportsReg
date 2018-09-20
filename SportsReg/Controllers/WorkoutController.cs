fa07c3ceee63a2c44af7e247d36f4733a2fa0a6f
Ricardo Nunes <ricardo.nunes.isla@gmail.com>
2018-09-20 22:44:45 +0100

    Deleted ValuesController. Added InitialMigration. Finished the other endpoints. Added the DefaultConnection for the PostgreSQL database. Endpoints tested.
    Backend Api finished. Going to start development of the Front End

--- SportsReg/Controllers/WorkoutController.cs
+++ SportsReg/Controllers/WorkoutController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SportsReg.Data;
 using SportsReg.Models;
 
@@ -20,7 +21,7 @@ namespace SportsReg.Controllers
             _context = context;
         }
 
-        // GET: api/Workouts
+        // GET: api/Workout/GetWorkouts
         [HttpGet]
         [Route("GetWorkouts")]
         public IEnumerable<Workout> GetWorkouts()
@@ -29,7 +30,7 @@ namespace SportsReg.Controllers
         }
 
 
-        // GET: api/Workout/1
+        // GET: api/Workout/GetWorkout/1
         [HttpGet]
         [Route("GetWorkout/{id}")]
         public async Task<IActionResult> GetWorkoutAsync(int id)
@@ -41,5 +42,50 @@ namespace SportsReg.Controllers
 
             return Ok(workout);
         }
+
+        // PUT: api/Workout/UpdateWorkout/1
+        [HttpPut]
+        [Route("UpdateWorkout/{id}")]
+        public async Task<IActionResult> UpdateWorkoutAsync(int id)
+        {
+            Workout workout = _context.Set<Workout>().Find(id);
+
+            if (workout == null)
+                return NotFound("$No workout with the id: {id}");
+
+            _context.Entry(workout).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Ok("Workout updated");
+        }
+
+        // POST: api/Workout/SaveWorkout
+        [HttpPost]
+        [Route("SaveWorkout")]
+        public async Task<IActionResult> SaveWorkout(Workout workout)
+        {
+            _context.Workouts.Add(workout);
+            await _context.SaveChangesAsync();
+
+            return Ok("Workout created");
+        }
+
+        // DELETE: api/Workout/DeleteWorkout/1
+        [HttpDelete]
+        [Route("DeleteWorkout/{1}")]
+        public async Task<IActionResult> DeleteWorkout(int id)
+        {
+            Workout workout = _context.Set<Workout>().Find(id);
+
+            if (workout == null)
+                return NotFound($"No workout with the id: {id}");
+
+            _context.Workouts.Remove(workout);
+            await _context.SaveChangesAsync();
+
+            return Ok("Workout deleted");
+        }
+
+        
     }
 }
---
+++ SportsReg/Migrations/20180920212513_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SportsReg.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Workouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Date = table.Column<DateTimeOffset>(nullable: false),
+                    DistanceInMeters = table.Column<int>(nullable: false),
+                    TimeInSeconds = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workouts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workouts");
+        }
+    }
+}
