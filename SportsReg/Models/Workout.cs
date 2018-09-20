bb723bb9ac303c71b9510099ddd829ab1fcfdedc
Ricardo Nunes <ricardo.nunes.isla@gmail.com>
2018-09-20 22:47:29 +0100

    Added Data Annotations

--- SportsReg/Models/Workout.cs
+++ SportsReg/Models/Workout.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,13 @@ namespace SportsReg.Models
     public class Workout
     {
         public int Id { get; set; }
+
+        [Required]
         public DateTimeOffset Date { get; set; }
+
         public int DistanceInMeters { get; set; }
+
+        [Required]
         public long TimeInSeconds { get; set; }
     }
 }
